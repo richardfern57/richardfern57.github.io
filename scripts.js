add3c2848cbbf12f447ea9f56e70119436b11d5c
Richard Fern <richard.fern@merton.ox.ac.uk>
2017-04-19 15:44:48 +0100

    fading

--- scripts.js
+++ scripts.js
@@ -3,9 +3,11 @@ function toggle(id) {
 	var symb = document.getElementById(id+"s");
 	if(row.style.maxHeight == row.scrollHeight+"px") {
 		row.style.maxHeight = "0px";
+		row.style.opacity = "0";
 		symb.innerHTML = "+";
 	}
 	else {
+		row.style.opacity = "1";
 		row.style.maxHeight = row.scrollHeight+"px";
 		symb.innerHTML = "-";
 	}
