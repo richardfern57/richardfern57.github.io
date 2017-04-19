a39c43865a85ded2c51366972a21a5224dc160b7
Richard Fern <richard.fern@merton.ox.ac.uk>
2017-04-19 12:33:16 +0100

    Sliding transitions

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 function toggle(id) {
 	var row = document.getElementById(id);
 	var symb = document.getElementById(id+"s");
-	if(row.style.display == "table-row") {
-		row.style.display = "none";
+	if(row.style.maxHeight == row.scrollHeight+"px") {
+		row.style.maxHeight = "0px";
 		symb.innerHTML = "+";
 	}
 	else {
-		row.style.display = "table-row";
+		row.style.maxHeight = row.scrollHeight+"px";
 		symb.innerHTML = "-";
 	}
 }
